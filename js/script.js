ea8d9206ffac7a25ce5be2a46ce0c344090f0400
TubagusYogiS <t_suryaputra@yahoo.com>
2024-12-19 19:20:28 +0700

    Untuk di submit tugas Mini Project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,35 @@
 // Ini Javascript
+// untuk cek sudah terkoneksi atau belum -> console.log("Hello World!");
+console.log("Javascript berhasil tersambung");
+
+showBanner();
+
+//Fungsi untuk memvalidasi dari Javascript
+function validateForm() {
+    console.log("Validate form executed");
+    const usernameInput = document.getElementById("username-input").value;
+    console.log(usernameInput);
+
+//Fungsi untuk validasi nama user jika tidak kosong    
+    if (usernameInput == "") {
+        alert("Inputan tidak boleh kosong");
+    } else {
+        document.getElementById("username-result").innerHTML = usernameInput;
+    }
+
+    console.log("validateForm executed");
+    }
+
+function showBanner() {
+    const imageList = document.getElementsByClassName("banner-img")
+    console.log (imageList);
+
+    //Hide semua banner
+        for (let i = 0; i < imageList.length; i++) {
+        imageList.style.display = "none";
+        }
+
+    //Show 1 banner sesuai posisi yang diinginkan
+    imageList(i).style.display = "block";
+}
+//document.getElementById("submit-btn").addEventListener;
